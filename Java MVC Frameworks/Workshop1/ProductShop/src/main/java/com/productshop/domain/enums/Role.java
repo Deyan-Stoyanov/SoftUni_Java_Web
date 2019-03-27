46961037c464bb21d8a69187801bb45dd939aa73
Deyan-Stoyanov <deyan959@gmail.com>
2019-03-27 16:17:05 +0200

    Remove duplicate category

--- Java MVC Frameworks/Workshop1/ProductShop/src/main/java/com/productshop/domain/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.productshop.domain.enums;
-
-public enum Role {
-    ROLE_ROOT, ROLE_ADMIN, ROLE_MODERATOR, ROLE_USER
-}
--- Java MVC Frameworks/Workshop1/ProductShop/src/main/java/com/productshop/service/UserRoleService.java
+++
@@ -1,8 +0,0 @@
-package com.productshop.service;
-
-import com.productshop.domain.entity.UserRole;
-import com.productshop.domain.enums.Role;
-
-public interface UserRoleService {
-    UserRole findByRole(Role role);
-}
